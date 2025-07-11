583cffc57b3b3ab6a3bfa325ec9454a65c0dc0a1
Pinninti Pavan <pinnintipavan04@gmail.com>
2025-07-11 22:37:44 +0530

    Setup vapi and header

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -2,7 +2,7 @@
 
 import { auth } from "@clerk/nextjs/server"
 import { createSupabaseClient } from "../supabase"
-import { queryObjects } from "v8";
+
 
 export const createCompanion = async (formData: CreateCompanion) => {
     const {userId: author} = await auth()
@@ -35,4 +35,14 @@ export const getAllCompanions = async ({limit = 10, page = 1, subject, topic}: G
     if(error) throw new Error(error.message);
 
     return companions;
+}
+
+export const getCompanion = async(id: string) => {
+    const supabase = createSupabaseClient();
+
+    const {data, error} = await supabase.from('companions').select().eq('id', id);
+
+    if(error) return console.log(error);
+
+    return data[0]
 }
