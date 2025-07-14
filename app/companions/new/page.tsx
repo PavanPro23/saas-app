bed0730d79aebe17fd8786849930280e50bda477
Pinninti Pavan <pinnintipavan04@gmail.com>
2025-07-14 21:26:57 +0530

    Implemented Clerk Subscriptions

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -2,7 +2,7 @@ import CompanionForm from '@/components/CompanionForm'
 import { newCompanionPermissions } from '@/lib/actions/companion.actions'
 import { auth } from '@clerk/nextjs/server'
 import Image from 'next/image'
-import Link from 'next/navigation/link'
+import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
 const NewCompanion = async () => {
